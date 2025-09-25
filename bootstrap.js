a14471393b981264784403ca45608611a42f4a93
Jamol <jam35d12@gmail.com>
2025-09-25 18:22:51 +0500

    Initial commit

---
+++ bootstrap.js
@@ -0,0 +1,11 @@
+import e from "express"
+const app = e()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
